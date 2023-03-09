c4e299a358a6305cfec6649549d1e87efbef3050
nicho <nicholas.tayag@ufl.edu>
2023-03-09 15:37:37 -0500

    a

--- lab6.py
+++ lab6.py
@@ -3,7 +3,7 @@
 def encode(password):
     new_password = ""
     for i in password:
-        num_3 = int(i) + 3
+        num_3 = int(i)+3
         new_password += str(num_3)
-    return password
+    return new_password
 
--- main.py
+++ main.py
@@ -3,17 +3,24 @@ from decode import decode
 
 def menu():
     print("Menu")
-    print("-------------")
-    print("Type '1' to Encode")
-    print("Type '2' to Decode")
-
+    print("------------- ")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
 
 while True:
     menu()
     option = int(input())
     if option == 1:
+        password = input("Please enter your password to encode: ")
         new_password = encode(password)
-        print(new_password)
+        print("Your password has been encoded and stored!")
     if option == 2:
-        decode(new_password)
+        decodedpassword = decode(encodepass)
+        print(f"The encoded password is {new_password}, and the original password is {decodepassword}")
+    if option == 3:
+        break
+
+
+
 
